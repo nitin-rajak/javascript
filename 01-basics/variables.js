4ae10878f1192dd8d5f0371067fa849ce4840a11
Nitin Rajak <nitinrajak410@gmail.com>
2026-06-22 04:27:38 +0000

    done

--- 01-basics/variables.js
+++ 01-basics/variables.js
@@ -2,7 +2,15 @@ const accountId = 144553
 let accountEmail = "nitin@google.com"
 var accountPassword = "1234"
 accountCity = "Rewa"
+let accountState;
 
 // accountId = 2 // not allowed
 
+accountEmail = "rajak@no.com"
+accountPassword = "12344545"
+accountCity = "mumbai"
+
 console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
